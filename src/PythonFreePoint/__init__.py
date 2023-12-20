31ae9abc11eba81703aa028bac248a10d8adf966
itruffat <imtruffat@gmail.com>
2023-12-20 22:15:26 +1100

    One more fix

--- src/PythonFreePoint/__init__.py
+++ src/PythonFreePoint/__init__.py
@@ -1,1 +1,1 @@
-from  main import pointfree, _pointfree, _pointfree_with_vars, PointFreeFunction, NotPointFreeError, InvalidCombinationError
+from .main import pointfree, _pointfree, _pointfree_with_vars, PointFreeFunction, NotPointFreeError, InvalidCombinationError
